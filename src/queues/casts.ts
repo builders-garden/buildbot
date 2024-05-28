7f23793676438c58d8e533a09d520946cdcf72ad
PaoloRollo <paolo.rollo1997@gmail.com>
2024-05-28 17:20:10 +0200

    feat: add rate-limit to jobs.

--- src/queues/casts.ts
+++ src/queues/casts.ts
@@ -3,6 +3,7 @@ import { MessageBody } from "../schemas.js";
 import { publishCast } from "../utils/index.js";
 import { env } from "../env.js";
 import { redisConnection } from "./connection.js";
+import { AxiosError } from "axios";
 
 const CASTS_QUEUE_NAME = "casts";
 
@@ -15,34 +16,49 @@ export const processCast = async (job: { data: MessageBody }) => {
 
   console.log(`[casts worker] [${Date.now()}] - new cast received. iterating.`);
 
-  try {
-    const hash = await publishCast(text, {
-      replyTo: env.FARCASTER_REPLY_TO_CAST_HASH,
-    });
-    console.log(
-      `[casts worker] [${Date.now()}] - cast ${hash} published successfully .`
-    );
-  } catch (error) {
-    console.error(
-      `[casts worker] [${Date.now()}] - error publishing cast: ${
-        error instanceof Error ? error.message : "unknown error"
-      }.`
-    );
-  }
+  const hash = await publishCast(text, {
+    replyTo: env.FARCASTER_REPLY_TO_CAST_HASH,
+  });
+  console.log(
+    `[casts worker] [${Date.now()}] - cast ${hash} published successfully .`
+  );
 };
 
 if (env.REDIS_HOST) {
   // @ts-ignore
-  const castsWorker = new Worker(CASTS_QUEUE_NAME, processCast, {
-    connection: redisConnection,
-    metrics: {
-      maxDataPoints: MetricsTime.ONE_WEEK,
+  const castsWorker = new Worker(
+    CASTS_QUEUE_NAME,
+    async (job: { data: MessageBody }) => {
+      try {
+        await processCast(job);
+      } catch (error) {
+        if (error instanceof AxiosError) {
+          if (error.response?.status === 429) {
+            console.log(
+              `[casts worker] [${Date.now()}] - rate limited, trying later.`
+            );
+            await castsWorker.rateLimit(1000);
+            throw Worker.RateLimitError();
+          }
+        }
+        console.error(
+          `[casts worker] [${Date.now()}] - error publishing cast: ${
+            error instanceof Error ? error.message : "unknown error"
+          }.`
+        );
+      }
     },
-    limiter: {
-      max: 1,
-      duration: 1000,
-    },
-  });
+    {
+      connection: redisConnection,
+      metrics: {
+        maxDataPoints: MetricsTime.ONE_WEEK,
+      },
+      limiter: {
+        max: 1,
+        duration: 1000,
+      },
+    }
+  );
 }
 
 export const castsQueue = env.REDIS_HOST
