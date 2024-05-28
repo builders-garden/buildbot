3f334cf3e43f7c73ec3a0b486a17742490fc3eee
PaoloRollo <paolo.rollo1997@gmail.com>
2024-05-28 17:36:23 +0200

    feat: add slow down to APIs.

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,18 @@ import { env } from "./env.js";
 import express from "express";
 import { utilsRouter, webhooksRouter } from "./routes/index.js";
 import { startJobs } from "./jobs/index.js";
+import slowDown from "express-slow-down";
 
 export const app = express();
 
+const apiLimiter = slowDown({
+  windowMs: 60 * 1000, // 1 minute
+  delayAfter: 200, // Allow only 200 requests per minute to go at full speed.
+  delayMs: (hits) => 5000, // Slow down each request after the limit by 1 second.
+});
+
 app.use(express.json({ limit: "10mb" }));
+app.use(apiLimiter);
 app.use("/", utilsRouter);
 app.use("/webhooks", webhooksRouter);
 
