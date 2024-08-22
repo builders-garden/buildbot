64c39fd94efee034ea4d745519d4bfe7b4ecc0c9
Matteo Midena <matteo.midena@protonmail.com>
2024-08-22 13:25:42 +0100

    fix: increased limit params for replies worker queue + add some metrics

--- src/routes/utils/metrics.ts
+++ src/routes/utils/metrics.ts
@@ -1,40 +1,56 @@
 import { Request, Response } from "express";
-import { castsQueue } from "../../queues/casts.js";
+// import { castsQueue } from "../../queues/casts.js";
 import { dcsQueue } from "../../queues/dcs.js";
 import { xmtpQueue } from "../../queues/xmtp.js";
+import { repliesQueue } from "../../queues/replies.js";
 
 export const metricsHandler = async (_: Request, res: Response) => {
-  const [castsFailed, dcsFailed, xmtpFailed] = await Promise.all([
-    castsQueue?.getMetrics("failed"),
-    dcsQueue?.getMetrics("failed"),
-    xmtpQueue?.getMetrics("failed"),
-  ]);
+  // const [
+  //   // castsFailed,
+  //   repliesFailed,
+  //   repliesCompleted,
+  //   dcsFailed,
+  //   dcsCompleted,
+  //   xmtpFailed,
+  //   xmtpCompleted,
+  // ] = await Promise.all([
+  //   // castsQueue?.getMetrics("failed"),
+  //   repliesQueue?.getMetrics("failed"),
+  //   repliesQueue?.getMetrics("completed"),
+  //   dcsQueue?.getMetrics("failed"),
+  //   dcsQueue?.getMetrics("completed"),
+  //   xmtpQueue?.getMetrics("failed"),
+  //   xmtpQueue?.getMetrics("completed"),
+  // ]);
 
   res.status(200).send({
     result: {
-      casts: castsFailed
-        ? {
-            completed: castsFailed!.meta.count - castsFailed!.count,
-            failed: castsFailed?.count,
-            performance:
-              100 - (castsFailed!.count / castsFailed!.meta.count) * 100,
-          }
-        : null,
-      dcs: dcsFailed
-        ? {
-            completed: dcsFailed!.meta.count - dcsFailed!.count,
-            failed: dcsFailed?.count,
-            performance: 100 - (dcsFailed!.count / dcsFailed!.meta.count) * 100,
-          }
-        : null,
-      xmtp: xmtpFailed
-        ? {
-            completed: xmtpFailed!.meta.count - xmtpFailed!.count,
-            failed: xmtpFailed?.count,
-            performance:
-              100 - (xmtpFailed!.count / xmtpFailed!.meta.count) * 100,
-          }
-        : null,
+      // casts: castsFailed
+      //   ? {
+      //       completed: castsFailed!.meta.count - castsFailed!.count,
+      //       failed: castsFailed?.count,
+      //       performance:
+      //         100 - (castsFailed!.count / castsFailed!.meta.count) * 100,
+      //     }
+      //   : null,
+      replies: {
+        completed: await repliesQueue?.getCompletedCount(),
+        failed: await repliesQueue?.getFailedCount(),
+        waiting: await repliesQueue?.getWaitingCount(),
+        delayed: await repliesQueue?.getDelayedCount(),
+      },
+      dcs: {
+        completed: await dcsQueue?.getCompletedCount(),
+        failed: await dcsQueue?.getFailedCount(),
+        waiting: await dcsQueue?.getWaitingCount(),
+        delayed: await dcsQueue?.getDelayedCount(),
+      },
+      xmtp: {
+        completed: await xmtpQueue?.getCompletedCount(),
+        failed: await xmtpQueue?.getFailedCount(),
+        waiting: await xmtpQueue?.getWaitingCount(),
+        delayed: await xmtpQueue?.getDelayedCount(),
+      },
     },
   });
 };
