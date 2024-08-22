48e5a57a488b24235d2ce4e6b0229cf4483d3ceb
Matteo Midena <matteo.midena@protonmail.com>
2024-08-22 10:55:30 +0100

    chore: removed unused imports

--- src/routes/webhooks/mentions.ts
+++ src/routes/webhooks/mentions.ts
@@ -1,10 +1,13 @@
 import { Request, Response } from "express";
-import { MentionsBody, SimpleCastBody } from "../../schemas.js";
+import {
+  MentionsBody,
+  // SimpleCastBody
+} from "../../schemas.js";
 import {
   getFarcasterUsersByAddresses,
   isCorrectUser,
 } from "../../utils/index.js";
-import { addToCastsQueue } from "../../queues/index.js";
+// import { addToCastsQueue } from "../../queues/index.js";
 
 /**
  * @dev this function handles the mentions webhook
